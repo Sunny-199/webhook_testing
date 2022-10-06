8cd7a30068f72f08ec126efd73a15ec1c3b3fdcc
sunny <sunnyjaryal11@gmail.com>
2022-10-06 14:51:26 +0530

    initial commit

---
+++ myproject/core/views.py
@@ -0,0 +1,26 @@
+from django.urls import reverse_lazy
+from django.shortcuts import HttpResponseRedirect
+from django.views.generic import TemplateView
+from .forms import EmailForm
+from .mixins import EmailMixin
+
+
+class Index(EmailMixin, TemplateView):
+    template_name = 'index.html'
+    email_template_name = 'email.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(Index, self).get_context_data(**kwargs)
+        form = EmailForm()
+        data = dict()
+        data['email_to'] = self.request.POST.get('email_to')
+        data['title'] = self.request.POST.get('title')
+        data['message'] = self.request.POST.get('message')
+
+        context['form'] = form
+        context['data'] = data
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.send_mail()
+        return HttpResponseRedirect(reverse_lazy('core:index'))
