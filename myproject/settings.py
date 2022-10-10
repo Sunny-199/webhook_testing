3e4be34a2a5352796bb7ab890796916842b2261c
sunny <sunnyjaryal11@gmail.com>
2022-10-10 12:51:13 +0530

    setup email

--- myproject/settings.py
+++ myproject/settings.py
@@ -2,6 +2,8 @@ from pathlib import Path
 import os
 from decouple import config, Csv
 from dj_database_url import parse as dburl
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -106,3 +108,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+django_heroku.settings(locals())
